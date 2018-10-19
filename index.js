a4000e8ed8e37f4ec161fb654c6f51a306310399
heha-proglea <heha965+proglea@gmail.com>
2018-10-19 21:47:56 +0000

    update: write comments

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 'use strict';
+
+// このjavascriptファイルは、タスク管理を行うパッケージです
+
 // key: タスクの文字列 value: 完了しているかどうかの真偽値
 let tasks = new Map();
 
-const fs = require('fs');
-const fileName = './tasks.json';
+const fs = require('fs'); // ファイルシステムのモジュールの読み込み
+const fileName = './tasks.json'; // 書き込みを行うファイル名の宣言
+
 
 // 同期的にファイルから復元
-try {
-	const data = fs.readFileSync(fileName, 'utf8');
-	tasks = new Map(JSON.parse(data));
-} catch (ignore) {
-	console.log(fileName + 'から復元できませんでした');
+try { // try...catch文
+    const data = fs.readFileSync(fileName, 'utf8'); // ファイルから文字列を読み込み、dataに渡す
+    tasks = new Map(JSON.parse(data)); // 上記のdataをJSON.parse関数で解釈し、連想配列に変換する
+} catch (ignore) { // ファイルが存在しないなどのエラーが発生した場合の処理。このignoreには、発生したエラーが渡される
+    console.log(fileName + 'から復元できませんでした');
 }
 
 /**
  * タスクをファイルに保存する
  */
 function saveTasks() {
-	fs.writeFileSync(fileName, JSON.stringify(Array.from(tasks)), 'utf8');
+    fs.writeFileSync(fileName, JSON.stringify(Array.from(tasks)), 'utf8');
+    // 連想配列tasksをArray.formで配列に変換→JSON.stringifyでJSONの文字列に変換→同期的にファイル書き出し
 }
 
+
 /**
- * TODOを追加する
- * @param {string} task
- */
+* TODOを追加する
+* @param {string} task
+*/
 function todo(task) {
-	tasks.set(task, false);
-	saveTasks();
+    tasks.set(task, false);
+    saveTasks();
 }
 
 /**
@@ -35,7 +41,7 @@ function todo(task) {
  * @return {boolean} 完了したかどうか
  */
 function isDone(taskAndIsDonePair) {
-	return taskAndIsDonePair[1];
+    return taskAndIsDonePair[1];
 }
 
 /**
@@ -44,7 +50,7 @@ function isDone(taskAndIsDonePair) {
  * @return {boolean} 完了していないかどうか
  */
 function isNotDone(taskAndIsDonePair) {
-	return !isDone(taskAndIsDonePair);
+    return !isDone(taskAndIsDonePair);
 }
 
 /**
@@ -52,45 +58,46 @@ function isNotDone(taskAndIsDonePair) {
  * @return {array}
  */
 function list() {
-	return Array.from(tasks)
-		.filter(isNotDone)
-		.map(t => t[0]);
+    return Array.from(tasks) // 連想配列tasksを、配列に変換
+        .filter(isNotDone)
+        .map(t => t[0]);
 }
 
 /**
- * TODOを完了状態にする
- * @param {string} task
- */
+* TODOを完了状態にする
+* @param {string} task
+*/
 function done(task) {
-	if (tasks.has(task)) {
-		tasks.set(task, true);
-		saveTasks();
-	}
+    if (tasks.has(task)) {
+        tasks.set(task, true);
+        saveTasks();
+    }
 }
 
 /**
- * 完了済みのタスクの一覧の配列を取得する
- * @return {array}
- */
+* 完了済みのタスクの一覧の配列を取得する
+* @return {array}
+*/
 function donelist() {
-	return Array.from(tasks)
-		.filter(isDone)
-		.map(t => t[0]);
+    return Array.from(tasks)
+        .filter(isDone)
+        .map(t => t[0]);
 }
 
 /**
- * 項目を削除する
- * @param {string} task
- */
+* TODOを削除する
+* @param {string} task
+*/
 function del(task) {
-	tasks.delete(task);
-	saveTasks();
+    tasks.delete(task);
+    saveTasks();
 }
 
+// npmパッケージにtodoメソッドを追加(=関数をモジュール化した)(←module.exportsオブジェクトのプロパティとして関数を登録してる)(todo関数、list関数をモジュールの関数として公開)
 module.exports = {
-	todo: todo,
-	list: list,
-	done: done,
-	donelist: donelist,
-	del: del
+    todo: todo,
+    list: list,
+    done: done,
+    donelist: donelist,
+    del: del
 };
--- test.js
+++ test.js
@@ -1,26 +1,31 @@
+// test.jsの使い方→モジュールのディレクトリにてnode testを実行し、AssertionErrorが発生が発生しなければテストは無事成功
 'use strict';
 
 const fs = require('fs');
 
-fs.unlink('./tasks.json', (err) => {
-    const todo = require('./index.js');
-    const assert = require('assert');
-
+fs.unlink('./tasks.json', (err) => { // 指定したファイルを削除し、コールバック関数を呼ぶ。ファイルが存在しない場合、引数errにはエラーが渡される(今回は使わない)
+    // 以下、ファイル削除後に呼び出されるテスト処理
+    
+    const assert = require('assert'); // node.jsに付属する簡易テスト用モジュールの、Assertモジュールを呼び出し。
+    const todo = require('./index.js'); // モジュールindex.jsを、相対パスで直接呼び出す(ちなみに、npmパッケージとして呼び出す場合にはパッケージ名で呼び出す)
+    // このrequireしたタイミングで、index.jsが読み込まれ、書いてあるプログラムが実行される。
+    
     // todo と list のテスト
     todo.todo('ノートを買う');
     todo.todo('鉛筆を買う');
     assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
-
+    // assert.deepEqualは、第一引数と第二引数を比較する。成功すれと何も起こらない。失敗するとAssertionErrorが発生。
+    
     // done と donelist のテスト
     todo.done('鉛筆を買う');
     assert.deepEqual(todo.list(), ['ノートを買う']);
     assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
-
+    
     // del のテスト
     todo.del('ノートを買う');
     todo.del('鉛筆を買う');
-    assert.deepEqual(todo.list(), []);
-    assert.deepEqual(todo.donelist(), []);
-
+    assert.deepEqual(todo.list(), []); // タスク一覧がどちらも空になっていることを確認
+    assert.deepEqual(todo.donelist(), []); // 同上
+    
     console.log('テストが正常に完了しました');
 });
